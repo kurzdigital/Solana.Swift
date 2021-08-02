b471d207b7fe343aa2d7306e090c2affc7cf4d2f
Alexander Manzer <alexander.manzer@kurzdigital.com>
2021-08-02 17:02:59 +0200

    Add commitment parameter to getTransaction

--- Sources/Solana/Api/getTransaction/getTransaction.swift
+++ Sources/Solana/Api/getTransaction/getTransaction.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public extension Api {
-    func getTransaction(transactionSignature: String, onComplete: @escaping (Result<TransactionInfo, Error>) -> Void) {
-        router.request(parameters: [transactionSignature, "jsonParsed"]) { (result: Result<TransactionInfo, Error>) in
+    func getTransaction(transactionSignature: String, commitment: Commitment? = nil, onComplete: @escaping (Result<TransactionInfo, Error>) -> Void) {
+        router.request(parameters: [transactionSignature, "jsonParsed", RequestConfiguration(commitment: commitment)]) { (result: Result<TransactionInfo, Error>) in
             switch result {
             case .success(let transactions):
                 onComplete(.success(transactions))
