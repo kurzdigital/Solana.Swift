78e227d16b331427ee0e36bb4617f53dabc4640c
Alexander Manzer <alexander.manzer@kurzdigital.com>
2021-08-02 17:15:21 +0200

    Correct getTransaction call

--- Sources/Solana/Api/getTransaction/getTransaction.swift
+++ Sources/Solana/Api/getTransaction/getTransaction.swift
@@ -2,7 +2,8 @@ import Foundation
 
 public extension Api {
     func getTransaction(transactionSignature: String, commitment: Commitment? = nil, onComplete: @escaping (Result<TransactionInfo, Error>) -> Void) {
-        router.request(parameters: [transactionSignature, "jsonParsed", RequestConfiguration(commitment: commitment)]) { (result: Result<TransactionInfo, Error>) in
+        let configs = RequestConfiguration(commitment: commitment, encoding: "jsonParsed")
+        router.request(parameters: [transactionSignature, configs]) { (result: Result<TransactionInfo, Error>) in
             switch result {
             case .success(let transactions):
                 onComplete(.success(transactions))
