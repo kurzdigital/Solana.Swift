7f7318ef55d547e00da045cd3fae655b5ec82cb5
Alexander Manzer <alexander.manzer@kurzdigital.com>
2022-01-25 16:35:47 +0100

    Add GenesysGo Endpoint

--- Sources/Solana/Models/RPCEndpoint.swift
+++ Sources/Solana/Models/RPCEndpoint.swift
@@ -12,6 +12,7 @@ public struct RPCEndpoint: Hashable, Codable {
 
     public static let mainnetBetaSerum = RPCEndpoint(url: URL(string: "https://solana-api.projectserum.com")!, urlWebSocket: URL(string: "wss://solana-api.projectserum.com")!,  network: .mainnetBeta)
     public static let mainnetBetaSolana = RPCEndpoint(url: URL(string: "https://api.mainnet-beta.solana.com")!, urlWebSocket: URL(string: "wss://api.mainnet-beta.solana.com")!, network: .mainnetBeta)
+    public static let mainnetGenesysGo = RPCEndpoint(url: URL(string: "https://ssc-dao.genesysgo.net")!, urlWebSocket: URL(string: "wss://ssc-dao.genesysgo.net")!, network: .mainnetBeta)
     public static let devnetSolana = RPCEndpoint(url: URL(string: "https://api.devnet.solana.com")!, urlWebSocket: URL(string: "wss://api.devnet.solana.com")!, network: .devnet)
     public static let testnetSolana = RPCEndpoint(url: URL(string: "https://testnet.solana.com")!, urlWebSocket: URL(string: "wss://testnet.solana.com")!, network: .testnet)
 }
