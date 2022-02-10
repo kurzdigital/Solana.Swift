2e9fcdb666b3c4860a66e30bf4fe92773a0c0a54
Alexander Manzer <alexander.manzer@kurzdigital.com>
2022-02-10 17:34:47 +0100

    Comment getOrCreateAssociatedTokenAccount ftm

--- Sources/Solana/Actions/createAssociatedTokenAccount/createAssociatedTokenAccount.swift
+++ Sources/Solana/Actions/createAssociatedTokenAccount/createAssociatedTokenAccount.swift
@@ -2,55 +2,55 @@ import Foundation
 
 extension Action {
 
-    public func getOrCreateAssociatedTokenAccount(
-        owner: PublicKey,
-        tokenMint: PublicKey,
-        onComplete: @escaping (Result<(transactionId: TransactionID?, associatedTokenAddress: PublicKey), Error>) -> Void
-    ) {
-        guard case let .success(associatedAddress) = PublicKey.associatedTokenAddress(
-            walletAddress: owner,
-            tokenMintAddress: tokenMint
-        ) else {
-            onComplete(.failure(SolanaError.other("Could not create associated token account")))
-            return
-        }
-
-        api.getAccountInfo(
-            account: associatedAddress.base58EncodedString,
-            decodedTo: AccountInfo.self
-        ) { acountInfoResult in
-            switch acountInfoResult {
-            case .success(let info):
-                if info.owner == PublicKey.tokenProgramId.base58EncodedString &&
-                    info.data.value != nil {
-                    onComplete(.success((transactionId: nil, associatedTokenAddress: associatedAddress)))
-                    return
-                }
-                self.createAssociatedTokenAccount(
-                    for: owner,
-                    tokenMint: tokenMint
-                ) { createAssociatedResult in
-                    switch createAssociatedResult {
-                    case .success(let transactionId):
-                        onComplete(.success((transactionId: transactionId, associatedTokenAddress: associatedAddress)))
-                        return
-                    case .failure(let error):
-                        onComplete(.failure(error))
-                        return
-                    }
-                }
-            case .failure(let error):
-                onComplete(.failure(error))
-                return
-            }
-        }
-    }
+//    public func getOrCreateAssociatedTokenAccount(
+//        owner: PublicKey,
+//        tokenMint: PublicKey,
+//        onComplete: @escaping (Result<(transactionId: TransactionID?, associatedTokenAddress: PublicKey), Error>) -> Void
+//    ) {
+//        guard case let .success(associatedAddress) = PublicKey.associatedTokenAddress(
+//            walletAddress: owner,
+//            tokenMintAddress: tokenMint
+//        ) else {
+//            onComplete(.failure(SolanaError.other("Could not create associated token account")))
+//            return
+//        }
+//
+//        api.getAccountInfo(
+//            account: associatedAddress.base58EncodedString,
+//            decodedTo: AccountInfo.self
+//        ) { acountInfoResult in
+//            switch acountInfoResult {
+//            case .success(let info):
+//                if info.owner == PublicKey.tokenProgramId.base58EncodedString &&
+//                    info.data.value != nil {
+//                    onComplete(.success((transactionId: nil, associatedTokenAddress: associatedAddress)))
+//                    return
+//                }
+//                self.createAssociatedTokenAccount(
+//                    for: owner,
+//                    tokenMint: tokenMint
+//                ) { createAssociatedResult in
+//                    switch createAssociatedResult {
+//                    case .success(let transactionId):
+//                        onComplete(.success((transactionId: transactionId, associatedTokenAddress: associatedAddress)))
+//                        return
+//                    case .failure(let error):
+//                        onComplete(.failure(error))
+//                        return
+//                    }
+//                }
+//            case .failure(let error):
+//                onComplete(.failure(error))
+//                return
+//            }
+//        }
+//    }
 
     public func createAssociatedTokenAccount(
         for owner: PublicKey,
         tokenMint: PublicKey,
         payer: Account? = nil,
-        onComplete: @escaping ((Result<TransactionID, Error>) -> Void)
+        onComplete: @escaping (Result<(transactionId: TransactionID, associatedTokenAddress: PublicKey), Error>) -> Void
     ) {
         // get account
         guard let payer = try? payer ?? auth.account.get() else {
@@ -80,8 +80,8 @@ extension Action {
                 signers: [payer]
             ) { serializeResult in
                 switch serializeResult {
-                case .success(let reesult):
-                    onComplete(.success(reesult))
+                case .success(let transactionId):
+                    onComplete(.success((transactionId: transactionId, associatedTokenAddress: associatedAddress)))
                 case .failure(let error):
                     onComplete(.failure(error))
                     return
