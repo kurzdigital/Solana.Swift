4fb1705a4c00ca472ea793220dc53006a1859db7
Alexander Manzer <alexander.manzer@kurzdigital.com>
2022-02-11 14:07:43 +0100

    Add getTokenAccountInfo method

--- Sources/Solana/Api/getAccountInfo/getAccountInfo.swift
+++ Sources/Solana/Api/getAccountInfo/getAccountInfo.swift
@@ -16,4 +16,20 @@ public extension Api {
             }
         }
     }
+    
+    func getTokenAccountInfo(account: String, commitment: Commitment? = nil, onComplete: @escaping(Result<SolanaTokenAccount, Error>) -> Void) {
+        let configs = RequestConfiguration(commitment: commitment, encoding: "jsonParsed")
+        router.request(bcMethod: "getAccountInfo", parameters: [account, configs]) {  (result: Result<Rpc<SolanaTokenAccount?>, Error>) in
+            switch result {
+            case .success(let rpc):
+                guard let value = rpc.value else {
+                    onComplete(.failure(SolanaError.nullValue))
+                    return
+                }
+                onComplete(.success(value))
+            case .failure(let error):
+                onComplete(.failure(error))
+            }
+        }
+    }
 }
